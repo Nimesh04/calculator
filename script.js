e23df7091904aaa9cc81d0ca7853aeada5479a91
Nimesh04 <nimeshb907@gmail.com>
2025-07-15 14:10:25 -0500

    Completed the front-end design
    Originally all the buttons were being created with
    js code but now replaced it with html code to make
    it much easier to manage.

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
-const calculatorBody = document.querySelector(".calculator-body");
 
-let square;
-for(let i = 0; i<= 4;i++){
-    for(let j = 0; j<= 5; j++){
-        square = document.createElement("button");
-        calculatorBody.append(square);
-    }
-}
+
